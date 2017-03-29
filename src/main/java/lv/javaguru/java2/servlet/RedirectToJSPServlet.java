f83d2953eb4d993c101246ab46435ec7258a1bd8
Victor <viktor.antipin@gmail.com>
2017-03-29 14:13:06 +0300

    New tables added

--- src/main/java/lv/javaguru/java2/servlet/RedirectToJSPServlet.java
+++ src/main/java/lv/javaguru/java2/servlet/RedirectToJSPServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 public class RedirectToJSPServlet extends HttpServlet {
@@ -18,12 +19,14 @@ public class RedirectToJSPServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response) throws ServletException, IOException {
-        // Prepare output html
+        // greeting test
         UserService userService = new UserServiceImpl();
         Long userId = Long.valueOf(1003);
         Optional<User> userOpt = userService.showById(userId);
         request.setAttribute("userOpt", userOpt.get());
-
+        // show all users
+        List<User> users = userService.showAll();
+        request.setAttribute("users", users);
         ServletContext servletContext = getServletContext();
         RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher("/helloWorld.jsp");
         requestDispatcher.forward(request, response);
--- src/main/webapp/helloWorld.jsp
+++ src/main/webapp/helloWorld.jsp
@@ -1,4 +1,5 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title>JSP page</title>
@@ -6,6 +7,22 @@
 <body>
 
     <h1>Hello ${userOpt.firstName} ${userOpt.lastName} </h1>
+
+<h2> Users</h2>
+<table>
+    <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+    </tr>
+    <c:forEach var="tempUser" items="${users}">
+
+        <tr>
+            <td>${tempUser.firstName}</td>
+            <td>${tempUser.lastName}</td>
+        </tr>
+    </c:forEach>
+
+</table>
 
 </body>
 </html>
