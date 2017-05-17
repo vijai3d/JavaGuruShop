8b7cbb39a1009063b3ee35b6a2e44ba29bf22f52
Vijai3D <viktor.antipin@gmail.com>
2017-05-17 20:58:32 +0300

    Added customerDAO and Service

---
+++ database/data.sql
@@ -0,0 +1,42 @@
+INSERT INTO java_guru_shop2.category (name) VALUES ('CD');
+INSERT INTO java_guru_shop2.category (name) VALUES ('DVD');
+INSERT INTO java_guru_shop2.category (name) VALUES ('Books');
+INSERT INTO java_guru_shop2.category (name) VALUES ('Games');
+INSERT INTO java_guru_shop2.customers (name, email, phone, address, city, country, password) VALUES ('Viktor Antipin', 'vijai3d@inbox.lv', '23456778', 'Jaunsaules 18b', 'Riga', 'LATVIA', 'fgdfgdfd');
+INSERT INTO java_guru_shop2.hibernate_sequence (next_val) VALUES (1);
+INSERT INTO java_guru_shop2.product (name, price, description, last_update, category_id) VALUES ('God''s Problem Child', 9.99, 'Closing the album is "He Won''t Ever Be Gone," a song written by Gary Nicholson that pays tribute to Willie''s outlaw country comrade, Merle Haggard.', '2017-05-02 19:03:13', 1);
+INSERT INTO java_guru_shop2.product (name, price, description, last_update, category_id) VALUES ('Guardians Of The Galaxy Vol. 2', 9.38, 'Set to the all-new sonic backdrop of Awesome Mixtape #2, Marvel Studios'' "Guardians of the Galaxy Vol. 2" continues the team''s adventures as they traverse the outer reaches of the cosmos.', '2017-05-02 20:07:31', 1);
+INSERT INTO java_guru_shop2.product (name, price, description, last_update, category_id) VALUES ('Humanz', 16.67, ' 1. Intro: I Switched My Robot Off
+  2. Ascension (feat. Vince Staples)
+  3. Strobelite (feat. Peven Everett)
+  4. Saturnz Barz (feat. Popcaan)
+  5. Momentz (feat. De La Soul)
+', '2017-05-02 20:10:11', 1);
+INSERT INTO java_guru_shop2.product (name, price, description, last_update, category_id) VALUES ('Strength Of A Woman', 9.99, 'Strength of a Woman, the new album by MJB, includes Thick Of It , U + Me (Love Lessons) & Love Yourself ft. Kayne West which make-up her most honest body of work to date.
+', '2017-05-02 20:11:12', 1);
+INSERT INTO java_guru_shop2.product (name, price, description, last_update, category_id) VALUES ('Trolls', 17.86, 'Trolls boy ray/DVD/ultraviolet HD combo pack
+', '2017-05-02 20:13:54', 2);
+INSERT INTO java_guru_shop2.product (name, price, description, last_update, category_id) VALUES ('Storks', 12.99, 'Storks deliver babies…or at least they used to. Now they deliver packages for global internet giant Cornerstore.com. 
+', '2017-05-02 20:15:47', 2);
+INSERT INTO java_guru_shop2.product (name, price, description, last_update, category_id) VALUES ('Finding Dory', 18.99, 'From the Academy Award(R)-winning creators of Disney-Pixar''s FINDING NEMO (Best Animated Feature, 2003) comes an epic undersea adventure filled with imagination, humor and heart. 
+', '2017-05-02 20:16:40', 2);
+INSERT INTO java_guru_shop2.product (name, price, description, last_update, category_id) VALUES ('Moana', 19.10, 'From Walt Disney Animation Studios comes MOANA, an epic adventure about a spirited teen who sets sail on a daring mission to save her people. Along the way, Moana (Auli''i Cravalho) meets the once mighty demigod Maui (Dwayne Johnson)
+', '2017-05-02 20:25:52', 2);
+INSERT INTO java_guru_shop2.product (name, price, description, last_update, category_id) VALUES ('Java WebSocket Programming (Oracle Press) ', 7.63, 'Build dynamic enterprise Web applications that fully leverage state-of-the-art communication technologies. 
+', '2017-05-02 20:29:05', 3);
+INSERT INTO java_guru_shop2.product (name, price, description, last_update, category_id) VALUES ('Spring Microservices', 44.99, 'Learn how to efficiently build and implement microservices in Spring, and how to use Docker and Mesos to push the boundaries of what you thought possible
+', '2017-05-02 20:30:01', 3);
+INSERT INTO java_guru_shop2.product (name, price, description, last_update, category_id) VALUES ('JavaFX Essentials', 34.99, 'Develop amazing gestures –based applications and an interactive JavaFX application powered by leap motion devices
+', '2017-05-02 20:31:02', 3);
+INSERT INTO java_guru_shop2.product (name, price, description, last_update, category_id) VALUES ('Java Network Programming', 23.99, 'This practical guide provides a complete introduction to developing network programs with Java. You’ll learn how to use Java’s network class library to quickly and easily accomplish 
+', '2017-05-02 20:33:54', 3);
+INSERT INTO java_guru_shop2.product (name, price, description, last_update, category_id) VALUES ('Mario Kart 8 Deluxe - Nintendo Switch', 59.88, 'Hit the road with the definitive version of Mario Kart 8 and play anytime, anywhere! Race your friends or battle them in a revised battle mode on new and returning battle courses.
+', '2017-05-02 20:37:10', 3);
+INSERT INTO java_guru_shop2.product (name, price, description, last_update, category_id) VALUES ('Mario Kart 8 Deluxe - Nintendo Switch', 59.88, 'Hit the road with the definitive version of Mario Kart 8 and play anytime, anywhere! Race your friends or battle them in a revised battle mode on new and returning battle courses.
+', '2017-05-02 20:38:49', 4);
+INSERT INTO java_guru_shop2.product (name, price, description, last_update, category_id) VALUES ('The Legend of Zelda: Breath of the Wild', 52.88, 'Forget everything you know about The Legend of Zelda games.
+', '2017-05-02 20:41:38', 4);
+INSERT INTO java_guru_shop2.product (name, price, description, last_update, category_id) VALUES ('Crash Bandicoot N. Sane Trilogy', 37.62, 'Forget everything you know about The Legend of Zelda games.
+', '2017-05-02 20:48:04', 4);
+INSERT INTO java_guru_shop2.product (name, price, description, last_update, category_id) VALUES ('Final Fantasy XII The Zodiac Age', 49.99, 'Forget everything you know about The Legend of Zelda games.
+', '2017-05-02 20:48:36', 4);
